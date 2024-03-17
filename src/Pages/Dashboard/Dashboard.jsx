4a8e14f057c8a12da8eb896a309673bb5c9f12f1
Nadir Hossain <nadirhossain336@gmail.com>
2024-03-17 12:34:35 +0600

    logo change

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { SettingOutlined } from "@ant-design/icons";
-import { Button, Input, Layout, Menu, Badge, Switch } from "antd";
+import { Input, Layout,  Badge, Switch } from "antd";
 import React, { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
-import Logo from "../../Images/LOGO-1.png";
-import Styles from "./Dashboard.module.css";
-import { FaRegBell } from "react-icons/fa6";
-import { AiOutlineUser } from "react-icons/ai";
+import Logo from "../../assets/logo.jpg";
+import LogoText from "../../assets/logo-text.jpg";
 import { HiLogout, HiOutlineMail } from "react-icons/hi";
 import { LuUser } from "react-icons/lu";
 import { TbUserPlus } from "react-icons/tb";
@@ -14,151 +12,17 @@ import { MdDashboard } from "react-icons/md";
 import { FiSearch } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
 import { RiNotification2Line, RiChat1Line, RiCopperDiamondLine } from "react-icons/ri";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 const { Header, Sider, Content } = Layout;
 
-
-const items = [...Array(5).keys()].map((item, index) => {
-  return {
-    key: index,
-    label: (
-      <Link to="/notification" style={{}} rel="noreferrer">
-        <div
-          className={Styles.everyNotify}
-          style={{ display: "flex", alignItems: "center" }}
-        >
-          <img
-            style={{
-              backgroundColor: "#d9cffb",
-              borderRadius: "100%",
-              padding: "5px",
-              marginRight: "15px",
-            }}
-            width="30"
-            height="30"
-            src="https://img.icons8.com/3d-fluency/94/person-male--v2.png"
-            alt="person-male--v2"
-          />
-          <div className="" style={{ marginTop: "" }}>
-            <p>
-              <span>Sanchej haro manual </span>started a new trip from mexico.
-            </p>
-            <span style={{ color: "#d2d2d2" }}>1 hr ago</span>
-          </div>
-        </div>
-      </Link>
-    ),
-  };
-});
 
 const Dashboard = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  const [collapsed, setCollapsed] = useState(false);
 
-  const menu = (
-    <Menu>
-      <Menu.Item disabled>
-        <h2
-          style={{
-            color: "black",
-            fontWeight: "500",
-            borderBottom: "1px solid #e6e7f4",
-            paddingBottom: "20px",
-          }}
-        >
-          Notifications
-        </h2>
-        {/* <span style={{ fontWeight: 'bold', color: '#000' }}>Notifications</span> */}
-      </Menu.Item>
-      {items.map((item) => (
-        <Menu.Item key={item.key}>{item.label}</Menu.Item>
-      ))}
-      <div
-        className=""
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "15px",
-        }}
-      >
-        <Button
-          type="primary"
-          block
-          style={{
-            height: "50px",
-            backgroundColor: "#ffb7d5 ",
-            color: "white",
-            fontSize: "18px",
-            fontWeight: "bold",
-          }}
-        >
-          <Link to="/notification">See All</Link>
-        </Button>
-      </div>
-    </Menu>
-  );
   const handleLogOut=()=>{
     navigate('/');
     window.location.reload();
   }
-
-  const profileItems = [
-    {
-      key: 1,
-      label: (
-        <Link to="/setting/personal-information" rel="noreferrer" >
-          <div
-            className={Styles.everyNotify}
-            style={{ display: "flex", alignItems: "center", gap: "15px"}}
-          >
-            <AiOutlineUser size={25} color="black" />
-            <div className="" style={{ marginTop: "0" }}>
-              <p>Profile</p>
-            </div>
-          </div>
-        </Link>
-      ),
-    },
-    {
-      key: 2,
-      label: (
-        <Link to="/notification" style={{}} rel="noreferrer">
-          <div
-            className={Styles.everyNotify}
-            style={{ display: "flex", alignItems: "center" ,gap: "15px" }}
-          >
-            <FaRegBell size={25} color="black" />
-            <div className="" style={{ marginTop: "" }}>
-              <p>Notification</p>
-            </div>
-          </div>
-        </Link>
-      ),
-    },
-    {
-      key: 3,
-      label: (
-        <div
-          style={{ border: "none", backgroundColor: "transparent" }}
-          rel="noreferrer"
-        >
-          <div
-            onClick={handleLogOut}
-            className={Styles.everyNotify}
-            style={{ display: "flex", alignItems: "center", gap: "15px", color: "black" }}
-          >
-            <HiLogout size={25} style={{color:"black"}}  />
-            <div  className="" style={{ marginTop: "" }}>
-              <p>Logout</p>
-            </div>
-          </div>
-        </div>
-      ),
-    },
-  ];
-
 
   const linkItems = [
     {
@@ -216,16 +80,29 @@ const Dashboard = () => {
             justifyContent: "center",
             alignItems: "center",
             marginTop: "30px",
-            marginBottom: "30px",
+            
           }}
         >
           <Link to="/">
             <img
               src={Logo}
-              height="150px"
-              width="150px"
+              height="50px"
+              width="50px"
             />
           </Link>
+        </div>
+
+        <div style={{
+          marginBottom: "30px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}> 
+          <img
+            src={LogoText}
+            height="50px"
+            width="50%"
+          />
         </div>
 
 
@@ -339,13 +216,12 @@ const Dashboard = () => {
                 borderRadius: "5px",
                 display: "flex", 
                 alignItems: "center",
-                justifyContent: "space-between",
+                gap: "20px",
                 padding: "10px"
               }}
             >
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLotvhr2isTRMEzzT30Cj0ly77jFThGXr0ng&usqp=CAU" style={{width: "40px", height: "40px", borderRadius: "100%"}} alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLotvhr2isTRMEzzT30Cj0ly77jFThGXr0ng&usqp=CAU" style={{width: "30px", height: "30px", borderRadius: "100%"}} alt="" />
               <h2 style={{color: "black", fontSize: "10px"}}>DR. Jim ahhmed</h2>
-              <MdOutlineKeyboardArrowDown size={18} color="#D9D9D9" />
             </div>
           </div>
         </Header>
